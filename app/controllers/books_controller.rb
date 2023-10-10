b09d98fd0fc85937eeac04d31a105d7aa6dbf813
azarJY <143927567+azarJY@users.noreply.github.com>
2023-10-10 08:43:12 +0000

    [途中]5-27課題3進捗47

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,16 +10,20 @@ class BooksController < ApplicationController
   end
 
   def show
-    @user = current_user
     @books = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+       flash[:notice] = "You have created book successfully."
+       redirect_to book_path(@book.id)
+    else
+       render :new
+    end
   end
 
   def edit
@@ -28,8 +32,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+       redirect_to book_path(book.id)
+    else
+       render :new
+    end
   end
 
   def destroy
