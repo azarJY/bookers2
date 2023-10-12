1c0358c20fd27797e146e91895beac2db183fe90
azarJY <143927567+azarJY@users.noreply.github.com>
2023-10-12 07:17:52 +0000

    [Finish]5-27課題3再提出2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
   def index
     @users = User.all
+    @book = Book.new
     @user = current_user
   end
 
